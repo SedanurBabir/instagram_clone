b3f7d188536b484bf5a99d80aab1a086ba6ed0f7
Sedanur Babir <sedababir1@gmail.com>
2025-01-22 16:38:14 +0300

    Fixed errors caused by incorrect definition of useCallBack in useShowToast hooks

--- src/hooks/useShowToast.js
+++ src/hooks/useShowToast.js
@@ -5,8 +5,14 @@ const useShowToast = () => {
 
     // The useCallback hook is used to memoize the function so that it doesn't get recreated on every render.
     const showToast = useCallback((title, description, status) => {
-        toast({ title: title, description: description, status: status, duration: 3000, isClosable: true })
-    })[toast];
+        toast({
+            title: title,
+            description: description,
+            status: status,
+            duration: 3000,
+            isClosable: true
+        });
+    }, [toast]);
     return showToast;
 }
 
